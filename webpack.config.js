06fbbe9e65e88b04a2a55981924d9db8b2fed376
Bignoux Didier <didieransleybignoux@live.com>
2020-07-13 09:09:26 +0400

    gulp install

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
-module.exports = function(_env, argv) {
+module.exports = function (_env, argv) {
   const isProduction = argv.mode === "production";
   const isDevelopment = !isProduction;
 
@@ -15,7 +16,7 @@ module.exports = function(_env, argv) {
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "assets/js/[name].[contenthash:8].js",
-      publicPath: "/"
+      publicPath: "/",
     },
     module: {
       rules: [
@@ -27,16 +28,16 @@ module.exports = function(_env, argv) {
             options: {
               cacheDirectory: true,
               cacheCompression: false,
-              envName: isProduction ? "production" : "development"
-            }
-          }
+              envName: isProduction ? "production" : "development",
+            },
+          },
         },
         {
           test: /\.css$/,
           use: [
             isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader"
-          ]
+            "css-loader",
+          ],
         },
         {
           test: /\.(png|jpg|gif)$/i,
@@ -44,46 +45,52 @@ module.exports = function(_env, argv) {
             loader: "url-loader",
             options: {
               limit: 8192,
-              name: "static/media/[name].[hash:8].[ext]"
-            }
-          }
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
         },
         {
           test: /\.svg$/,
-          use: ["@svgr/webpack"]
+          use: ["@svgr/webpack"],
         },
         {
           test: /\.(eot|otf|ttf|woff|woff2)$/,
           loader: require.resolve("file-loader"),
           options: {
-            name: "static/media/[name].[hash:8].[ext]"
-          }
-        }
-      ]
+            name: "static/media/[name].[hash:8].[ext]",
+          },
+        },
+      ],
     },
     resolve: {
-      extensions: [".js", ".jsx"]
+      extensions: [".js", ".jsx"],
     },
     plugins: [
       isProduction &&
         new MiniCssExtractPlugin({
           filename: "assets/css/[name].[contenthash:8].css",
-          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
         }),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public/index.html"),
-        inject: true
+        inject: true,
       }),
       new webpack.DefinePlugin({
         "process.env.NODE_ENV": JSON.stringify(
           isProduction ? "production" : "development"
-        )
+        ),
       }),
       new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-    })
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      }),
+      new FaviconsWebpackPlugin({
+        logo: path.join("assets/img/fav.png"),
+        prefix: "assets/img/fav-[hash]/",
+        emitStats: true,
+        statsFilename: "assets/img/fav.json",
+      }),
     ].filter(Boolean),
     optimization: {
       minimize: isProduction,
@@ -91,19 +98,19 @@ module.exports = function(_env, argv) {
         new TerserWebpackPlugin({
           terserOptions: {
             compress: {
-              comparisons: false
+              comparisons: false,
             },
             mangle: {
-              safari10: true
+              safari10: true,
             },
             output: {
               comments: false,
-              ascii_only: true
+              ascii_only: true,
             },
-            warnings: false
-          }
+            warnings: false,
+          },
         }),
-        new OptimizeCssAssetsPlugin()
+        new OptimizeCssAssetsPlugin(),
       ],
       splitChunks: {
         chunks: "all",
@@ -118,26 +125,22 @@ module.exports = function(_env, argv) {
                 /[\\/]node_modules[\\/](.*?)([\\/]|$)/
               )[1];
               return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-            }
+            },
           },
           common: {
             minChunks: 2,
-            priority: -10
-          }
-        }
+            priority: -10,
+          },
+        },
       },
-      runtimeChunk: "single"
+      runtimeChunk: "single",
     },
-   
+
     devServer: {
       compress: true,
       historyApiFallback: true,
       open: true,
-      overlay: true
+      overlay: true,
     },
-    
-    
-    
-    
   };
 };
